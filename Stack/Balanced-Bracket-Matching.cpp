e355786bae788ad018f518d7ca7545737ddee8b6
Khan Muhammad Rifat <140003326+khan1203@users.noreply.github.com>
2024-04-15 14:56:28 +0600

    Update Balanced-Bracket-Matching.cpp

--- Stack/Balanced-Bracket-Matching.cpp
+++ Stack/Balanced-Bracket-Matching.cpp
@@ -1,1 +1,48 @@
+/*
+Problem from HackerRank:
+Link: https://www.hackerrank.com/challenges/balanced-brackets/problem
+*/
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
 
+unordered_map<char, int> symbols = {{'[', -1}, {'{', -2}, {'(', -3}, {']', 1}, {'}', 2}, {')', 3}};
+string isBalanced(string s)
+{
+    stack<char> stc;
+    for (char bracket : s)
+    {
+        if (symbols[bracket] < 0)
+        {
+            stc.push(bracket);
+        }
+        else
+        {
+            if (stc.empty())
+                return "NO";
+            char top = stc.top();
+            stc.pop();
+            if (symbols[top] + symbols[bracket] != 0)
+            {
+                return "NO";
+            }
+        }
+    }
+    if (stc.empty())
+        return "YES";
+    return "NO";
+}
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    while (n--)
+    {
+        string str;
+        cin >> str;
+        cout << isBalanced(str) << "\n";
+    }
+    return 0;
+}
